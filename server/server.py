b53d89f772a888e1ccc6e6f4c0a6fc2c3d421902
nanino90 <ignacioutrilla@gmx.com>
2019-02-24 19:56:23 +0000

    Added service and improved web

--- server/server.py
+++ server/server.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python
+
 from flask import Flask, render_template
 import datetime
+import sys
+sys.path.insert(0, 'libLGTV_serial')
+from libLGTV_serial import LGTV
 
+#TV INIT
+model = '42LK450'
+serial_port = "/dev/ttyUSB0"
+tv = LGTV(model, serial_port)
+#SERVER INIT
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,15 +24,31 @@ def index():
 
    	return render_template('index.html', **templateData)
 
-@app.route("/<deviceName>/<action>")
+@app.route("/<deviceName>/<action>", methods=['GET'])
 def action(deviceName, action):
 	if deviceName == 'TV':
-   
 		if action == "on":
 			print "tv on"
+                        tv.send("poweron")
 		if action == "off":
 			print "tv off"
-   	
+                        tv.send("poweroff")
+		if action == "mute":
+			print "mute"
+                        tv.send("togglemute")
+                if action == "RPI":
+			print "Input RPI"
+                        tv.send("inputhdmi1")
+		if action == "tv":
+			print "input tv"
+                        tv.send("inputdigitalantenna")
+		if action == "up":
+			print "volume up"
+                        tv.send("volumeup")
+		if action == "down":
+			print "volume down"
+                        tv.send("volumedown")
+
 	return render_template('index.html')
 		     
 if __name__ == '__main__':
